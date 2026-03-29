407140da7cffbbb43a90fc47bdab3cf11810e8cf
Yurii Demyan <yra.demyan@gmail.com>
2026-03-29 14:17:56 +0300

    Complete task #1: Core Model and Enum setup

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProductsValidation.Models
 {
-    public class Product
+    public class Product : IValidatableObject // yura ts 1
     {
-        public enum Category { Toy, Technique, Clothes, Transport}
+        public enum Category { Toy, Technique, Clothes, Transport }
 
         public int Id { get; set; }
-        public Category Type { get; set; }
+
+        [Required(ErrorMessage = "Name is mandatory field")]
         public string Name { get; set; }
+
         public string Description { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "Price should be within boundaries 0 and 100000")]
         public decimal Price { get; set; }
+
+        [Required]
+        public Category Type { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var results = new List<ValidationResult>();
+
+            //  Slava (Task 2)
+
+            return results;
+        }
     }
 }
---
+++ Models/ProductType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations; // yura ts 1
+
+namespace ProductsValidation.Models
+{
+  
+    public enum ProductType
+    {
+        [Display(Name = "Toy")]
+        Toy,
+
+        [Display(Name = "Technique")]
+        Technique,
+
+        [Display(Name = "Clothes")]
+        Clothes,
+
+        [Display(Name = "Transport")]
+        Transport
+    }
+}
+// ts 1
