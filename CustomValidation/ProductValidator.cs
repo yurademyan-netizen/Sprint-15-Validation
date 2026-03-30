902a7ab98f54f88146f9a13d99f3fb38b15627e1
Loki22978964 <lokim3059@gmail.com>
2026-03-30 08:23:48 +0300

    added validation via fluentvalidation for the Description property

---
+++ CustomValidation/ProductValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using ProductsValidation.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace ProductsValidation.CustomValidation
+{
+    public class CustomValidation : AbstractValidator<Product>
+    {
+        public CustomValidation()
+        {
+            RuleFor(x => x.Description)
+            .MinimumLength(3)
+                .WithMessage("Description must be longer than 2 characters")
+
+            .Must((product, desc) => desc.StartsWith(product.Name))
+                .WithMessage("Description should start with the Name of the product")
+
+            .NotEqual(x => x.Name)
+                .WithMessage("Description should not be equal to Name")
+
+            .When(x => !string.IsNullOrEmpty(x.Description));
+        }
+    }
+}
